92e085b85d3e7329605d53ab26c909a625f55bf1
emily-wax <emily.wax@tamu.edu>
2023-02-11 18:39:36 +0000

    added link to nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_08_035517) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_11_182646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "budgets", force: :cascade do |t|
+    t.string "user"
+    t.string "expense_type"
+    t.float "amount"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
